514755cdb65aa143d5c95c2b5bf20d89ce7532d1
varkan15 <var.kandukuri@gmail.com>
2025-06-02 14:06:31 +0530

    tried adding button

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
 console.log("Hello from my portfolio!");
+
+const top = document.getElementById('top');
+
+window.addEventListener('scroll', () => { 
+    if (window.pageYOffset > 300) { 
+        top.classList.add('show');
+    } else { 
+        top.classList.remove('show');
+    }
+});
+
+top.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' 
+    });
+});
